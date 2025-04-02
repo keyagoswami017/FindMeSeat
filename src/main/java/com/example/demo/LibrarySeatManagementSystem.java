1f308106365f3d3f58a5744d3d38dda03b8ee8b2
keyagoswami017 <keyagoswami017@gmail.com>
2025-04-02 11:50:12 -0400

    Initial Setup for the project

---
+++ src/main/java/com/example/demo/LibrarySeatManagementSystem.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySeatManagementSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySeatManagementSystem.class, args);
+    }
+
+}
